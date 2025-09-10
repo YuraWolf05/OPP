d24c2a8ac72b51d6a6203b074cc7a5c1fc7c372e
YuraVolchkov <volchkov.yuraa@gmail.com>
2025-09-10 15:44:21 +0300

    Lab-1: Added Figure class

--- Volchkov/lab8vN/Lab-1/obj/Debug/net8.0/Lab-1.AssemblyInfo.cs
+++ Volchkov/lab8vN/Lab-1/obj/Debug/net8.0/Lab-1.AssemblyInfo.cs
@@ -23,7 +23,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab-1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60707124a99a9ec4d8cb385360a76b6185c78d99")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab-1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab-1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
