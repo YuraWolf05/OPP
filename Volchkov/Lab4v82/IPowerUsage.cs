9094cc83ff847fdbfb5b8a773492061afbac945a
YuraVolchkov <volchkov.yuraa@gmail.com>
2025-09-18 17:41:09 +0300

    ++

--- Volchkov/Lab4v82/IPowerUsage.cs
+++
@@ -1,9 +0,0 @@
-namespace Lab4
-{
-    // Інтерфейс для підрахунку споживання електроенергії
-    public interface IPowerUsage
-    {
-        double GetDailyConsumption();  // кВт·год за день
-        double GetWeeklyConsumption(); // кВт·год за тиждень
-    }
-}
